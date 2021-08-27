73a2e6eba5c477473640c530d0ed8c9bc919ebff
ue-sho <tssu45@gmail.com>
2021-08-27 21:33:05 +0900

    本番環境対応

--- manage.py
+++ manage.py
@@ -24,6 +24,8 @@ def setenv(variable, default):
     os.environ[variable] = os.getenv(variable, default)
 
 
+setenv("APPLICATION_CONFIG", "production")
+
 APPLICATION_CONFIG_PATH = "config"
 DOCKER_PATH = "docker"
 
@@ -155,6 +157,35 @@ def test(args):
 
     cmdline = docker_compose_cmdline("down")
     subprocess.call(cmdline)
+
+
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("subcommand", nargs=-1, type=click.Path())
+def compose(subcommand):
+    configure_app(os.getenv("APPLICATION_CONFIG"))
+    cmdline = docker_compose_cmdline() + list(subcommand)
+
+    try:
+        p = subprocess.Popen(cmdline)
+        p.wait()
+    except KeyboardInterrupt:
+        p.send_signal(signal.SIGINT)
+        p.wait()
+
+
+@cli.command()
+def init_postgres():
+    configure_app(os.getenv("APPLICATION_CONFIG"))
+
+    try:
+        run_sql([f"CREATE DATABASE {os.getenv('APPLICATION_DB')}"])
+    except psycopg2.errors.DuplicateDatabase:
+        print(
+            (
+                f"The database {os.getenv('APPLICATION_DB')} already",
+                "exists and will not be recreated",
+            )
+        )
 
 
 if __name__ == "__main__":
